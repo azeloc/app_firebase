82d35ff605c35c30f06c0cc793baa309b28199f6
Beatriz Milz <milz.bea@gmail.com>
2023-01-26 16:14:06 -0300

    arquivos html

--- login.R
+++ login.R
@@ -17,18 +17,54 @@ ui <- navbarPage(
                                   class = "imagemcentralizada"),
 
                 # FAZER LOGIN
-                shiny::HTML("<center><h4>Bem-vindo</h4></center>"),
-                shiny::HTML("<center>Fazer login no CIGA</center>"),
-                shiny::br(),
-                textInput("email", label = "Endereço de email: ", width = "100%"),
-                passwordInput("password", "Senha: ", width = "100%"),
-                actionButton("enviar", "Continuar", width = "100%", class = "btn-primary"),
-                shiny::br(), shiny::br(),
-                actionButton("esquecisenha", "Esqueceu a senha?", width = "100%", class = "btn-danger center"),
-                shiny::br(),shiny::br(),
-                actionButton("registrar", "Registrar", width = "100%", class = "btn-secondary align-self-center")
+               # shiny::HTML("<center><h4>Bem-vindo</h4></center>"),
+               # shiny::HTML("<center>Fazer login no CIGA</center>"),
+               # shiny::br(),
+               # textInput("email", label = "Endereço de email: ", width = "100%"),
+               # passwordInput("password", "Senha: ", width = "100%"),
+               # shiny::br(),shiny::br(),
+               # actionButton("enviar", "Continuar", width = "100%", class = "btn-primary", onclick = "login()"),
+               # shiny::br(), shiny::br(),
+               # a(
+               #  href = "recuperar-senha.html",
+               # actionButton("esquecisenha", "Esqueceu a senha?", width = "100%", class = "btn-danger center")
+               # ),
+               # shiny::br(), shiny::br(),
+               # a(
+               #   href = "cadastro.html",
+               #   shiny::actionButton("registrar", "Cadastro", width = "100%",
+               #                class = "btn-secondary align-self-center")
+               # )
+
 
                 # CADASTRE-SE
+                # shiny::HTML("<center><h4>Cadastre-se</h4></center>"),
+                # shiny::HTML("<center>Fazer o cadastro no CIGA</center>"),
+                # shiny::br(),
+                # textInput("email", label = "Endereço de email: ", width = "100%"),
+                # textInput("nome", label = "Nome: ", width = "100%"),
+                # textInput("sobrenome", label = "Sobrenome: ", width = "100%"),
+                # textInput("instituicao", label = "Instituição: ", width = "100%"),
+                # passwordInput("password", "Crie uma senha:", width = "100%"),
+                # shiny::br(),shiny::br(),
+                # actionButton("cadastro", "Cadastrar", width = "100%", class = "btn-primary", onclick = "register()"),
+                # shiny::br(), shiny::br(),
+                # shiny::actionButton("voltar", "Voltar", width = "100%",
+                #              class = "btn-secondary align-self-center", onclick = "history.back()")
+
+
+
+               # RECUPERAR SENHA
+               shiny::HTML("<center><h4>Recuperar a senha</h4></center>"),
+               shiny::HTML("<center>Recupere a senha para acessar o CIGA</center>"),
+               shiny::br(),
+               textInput("email", label = "Endereço de email: ", width = "100%"),
+               shiny::br(),shiny::br(),
+               actionButton("forgetpassword", "Recuperar a senha", width = "100%", class = "btn-primary", onclick = "forgetpassword()"),
+               shiny::br(), shiny::br(),
+               shiny::actionButton("voltar", "Voltar", width = "100%",
+                                   class = "btn-secondary align-self-center", onclick = "history.back()")
+
 
 
 
