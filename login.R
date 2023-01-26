702b01bb128b4a04198d2a20ab0f609025e3a8db
Beatriz Milz <milz.bea@gmail.com>
2023-01-26 14:02:27 -0300

    tela de login com bslib

--- login.R
+++ login.R
@@ -1,13 +1,31 @@
 library(shiny)
 
-ui <- fluidPage(
+ui <- navbarPage(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  theme = bslib::bs_theme(version = 5,
+                          bg = "#FFFFFF",
+                          fg = "#003b4d",
+                          primary = "#003b4d"),
   fluidRow(
-    column(
-      width = 4,
-      offset = 4,
-      textInput("email", label = "Email"),
-      passwordInput("password", "Senha"),
-      actionButton("enviar", "Enviar")
+    column(width = 4, offset = 4,
+    bslib::card(full_screen = FALSE,
+                bslib::card_header("Login"),
+                bslib::card_image(file = "www/CIGA_Simbolo-Contorno-85.svg",
+                                  width = "30%",
+                                  class = "imagemcentralizada"),
+                shiny::HTML("<center><h4>Bem-vindo</h4></center>"),
+                shiny::HTML("<center>Fazer login no CIGA</center>"),
+                shiny::br(),
+                textInput("email", label = "Endereço de email: ", width = "100%"),
+                passwordInput("password", "Senha: ", width = "100%"),
+                actionButton("enviar", "Continuar", width = "100%", class = "btn-primary"),
+                shiny::br(), shiny::br(),
+                actionButton("esquecisenha", "Esqueceu a senha?", width = "58%", class = "btn-danger center"),
+                #shiny::br(),shiny::br(),
+                actionButton("registrar", "Registrar", width = "40%", class = "btn-secondary align-self-center")
+     )
     )
   )
 )
