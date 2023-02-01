088c2391f3312fa7a16dbcdb467662ae3b3b8890
Fernando Corrêa <fptcorrea@gmail.com>
2023-02-01 18:59:41 -0300

    criação de usuario foi enviada pro backend

--- www/login.js
+++ www/login.js
@@ -15,15 +15,13 @@ login = function() {
 register = function() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  var nome = document.getElementById("name").value;
-  var adicional = document.getElementById("adicional").value;
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(function(user){
-    firebase.auth().currentUser.updateProfile({
-      displayName: nome,
-      department: adicional
-    })
-  }).catch(function(error){
-    alert("Falha ao logar");
-  });
+  var nome = document.getElementById("nome").value;
+  var sobrenome = document.getElementById("sobrenome").value;
+  var instituicao = document.getElementById("instituicao").value;
+
+  fetch("https://us-central1-exemplo-4d8da.cloudfunctions.net/criaUsuario?email="+email+"&nome="+nome+"&instituicao="+instituicao).then(function(response) {
+  alert("Usuário criado com sucesso! Aguarde a aprovação do seu cadastro ou entre em contato com ciga@aecom.com.");
+}).catch(function(err) {
+  alert("Erro na criação do usuário.");
+});
 }
