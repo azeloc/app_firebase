697bae5a42be74219e84e1a047a9079669a663c2
Beatriz Milz <milz.bea@gmail.com>
2023-02-01 18:39:41 -0300

    melhorando as mensagens de erro

--- www/login.js
+++ www/login.js
@@ -4,26 +4,53 @@ login = function() {
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function(user){
       firebase.auth().currentUser.getIdToken(true).then(function(token){
-      console.log(token);
       location.replace("http://127.0.0.1:4242/?t="+token)
 });
     }).catch(function(error){
-      alert("Erro ao fazer login. \n\nCaso seja necessário, entre em contato através do email: contato@ciga-mpmg.com.br ");
+     var codigoErro = error.code;
+     console.log(codigoErro);
+
+     if(codigoErro == "auth/user-not-found"){
+       var mensagemErro = "Usuário não encontrado. Verifique se o email está correto. Se necessário, solicite o cadastro novamente.";
+     } else {
+        var mensagemErro = error.message;
+        console.log(mensagemErro);
+     }
+
+      alert("Erro ao fazer login: \n\n" + mensagemErro + " \n\nCaso seja necessário, entre em contato através do email: contato@ciga-mpmg.com.br ");
     });
 }
 
 register = function() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  var nome = document.getElementById("name").value;
-  var adicional = document.getElementById("adicional").value;
+  var nome = document.getElementById("nome").value;
+  var sobrenome = document.getElementById("sobrenome").value;
+  var instituicao = document.getElementById("instituicao").value;
+  console.log(email);
+  console.log(nome);
+  console.log(sobrenome);
+  console.log(instituicao);
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(function(user){
     firebase.auth().currentUser.updateProfile({
       displayName: nome,
-      department: adicional
-    })
+      department: instituicao
+    });
+    alert("Cadastro solicitado com sucesso. \n\nCaso seja necessário, entre em contato através do email: contato@ciga-mpmg.com.br ");
+
   }).catch(function(error){
-      alert("Erro ao cadastrar. \n\nCaso seja necessário, entre em contato através do email: contato@ciga-mpmg.com.br ");
+      console.log(error);
+     var codigoErro = error.code;
+     console.log(codigoErro);
+
+     if(codigoErro == "auth/email-already-in-use"){
+       var mensagemErro = "Este email já está cadastrado.";
+     } else {
+        var mensagemErro = error.message;
+        console.log(mensagemErro);
+     }
+
+      alert("Erro ao solicitar cadastro: \n\n" + mensagemErro + " \n\nCaso seja necessário, entre em contato através do email: contato@ciga-mpmg.com.br ");
   });
 }
