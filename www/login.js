74c67884afb74ec2ac0bed1a62c47b9fdbdcebd8
Beatriz Milz <milz.bea@gmail.com>
2023-02-01 18:50:31 -0300

    merge e melhorias

--- www/login.js
+++ www/login.js
@@ -1,3 +1,60 @@
+function validarEmail(email) {
+
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (!email.match(validRegex)) {
+    alert("Endereço de e-mail inválido!");
+    document.getElementById("email").focus();
+    return false;
+  } else {
+    return true;
+  }
+
+}
+
+function validarNome(valor) {
+  if (valor.length == 0) {
+     alert("O campo Nome é obrigatório.");
+     document.getElementById("nome").focus();
+     return false;
+  } else {
+    return true;
+  }
+}
+
+function validarSobrenome(valor) {
+  if (valor.length == 0) {
+     alert("O campo Sobrenome é obrigatório.");
+     document.getElementById("sobrenome").focus();
+     return false;
+  } else {
+    return true;
+  }
+}
+
+function validarInstituicao(valor) {
+  if (valor.length == 0) {
+     alert("O campo Instituicao é obrigatório.");
+     document.getElementById("instituicao").focus();
+     return false;
+  } else {
+    return true;
+  }
+}
+
+function gerarSenha() {
+      var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+      var passwordLength = 16;
+      var password = "";
+
+      for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+      }
+     password
+}
+
+
 login = function() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
@@ -23,14 +80,17 @@ login = function() {
 
 register = function() {
   var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
   var nome = document.getElementById("nome").value;
   var sobrenome = document.getElementById("sobrenome").value;
   var instituicao = document.getElementById("instituicao").value;
+    if (validarEmail(email) && validarNome(nome) && validarSobrenome(sobrenome) &&
+  validarInstituicao(instituicao)){
   console.log(email);
+  console.log(password);
   console.log(nome);
   console.log(sobrenome);
   console.log(instituicao);
+  var password = String(gerarSenha());
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(function(user){
     firebase.auth().currentUser.updateProfile({
@@ -46,6 +106,8 @@ register = function() {
 
      if(codigoErro == "auth/email-already-in-use"){
        var mensagemErro = "Este email já está cadastrado.";
+     } else if(codigoErro == "auth/weak-password"){
+      var mensagemErro = "Senha fraca: a senha deve ter 6 caracteres ou mais.";
      } else {
         var mensagemErro = error.message;
         console.log(mensagemErro);
@@ -53,4 +115,7 @@ register = function() {
 
       alert("Erro ao solicitar cadastro: \n\n" + mensagemErro + " \n\nCaso seja necessário, entre em contato através do email: contato@ciga-mpmg.com.br ");
   });
+  }
 }
+
+
